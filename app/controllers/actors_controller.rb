5b5632703c74938f3627d4d920ecd47fd85277fa
Your Name <you@example.com>
2017-07-25 17:24:17 -0500

    commit temporal

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,13 @@ class ActorsController < ApplicationController
     def index
         @actors = Actor.all
     end
+
+    def new
+    end
+
+    def create
+    # puts params[:name]
+      actor = Actor.create(name: params[:name])
+      redirect_to '/actors'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     # resource :actors
     get '/actors', to: 'actors#index'
+    get '/actors/new', to: 'actors#new'
+    post '/actors', to: 'actors#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
